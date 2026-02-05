2bac1cb60309c4e743bfe3ac6f02001470121dce
Henry Bao <51253502+TheRealBaoBao@users.noreply.github.com>
2026-02-05 12:03:52 -0500

    Update l75.cpp

--- Sort Colors - Leetcode 75/l75.cpp
+++ Sort Colors - Leetcode 75/l75.cpp
@@ -19,6 +19,25 @@ public:
 
 };
 
+//quicksort partition
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, l = 0, r = nums.size() - 1;
+        while (i <= r) {
+            if (nums[i] == 0) {
+                swap(nums[l], nums[i]);
+                l++;
+            } else if (nums[i] == 2) {
+                swap(nums[i], nums[r]);
+                r--;
+                i--;
+            }
+            i++;
+        }
+    }
+};
+
 //Brute force O(nlogn)
 class Solution {
 public:
